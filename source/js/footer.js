85bdfbf5aa3d64532871663ea3bb978278e36a96
shundebo <105134757+shundebo@users.noreply.github.com>
2023-01-03 16:04:55 +0800

    首次更新

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2022-2023 br-get</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
